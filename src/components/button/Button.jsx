afd0f8ce929afbef2643ab75e687407477c5b148
deeepak-me <deepak.krishnan.e@gmail.com>
2023-06-27 02:49:11 +0530

    Implement button component

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./button.module.css";
+
+const Button = () => {
+  return <button className={styles.button}>See Product</button>;
+};
+
+export default Button;
