70fb7be0b3b5bb980e7acf3132dbbb31fdf864db
deeepak-me <deepak.krishnan.e@gmail.com>
2023-06-29 20:32:57 +0530

    feat: Updated with reusable button component

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./hero.module.css";
 import heroImg from "public/Bitmap.png";
+import Button from "../button/Button";
 
 export function Hero() {
   return (
@@ -12,7 +13,8 @@ export function Hero() {
           Experience natural, lifelike audio and exceptional build quality made
           for the passionate music enthusiast.
         </p>
-        <button className={styles.button}>See Product</button>
+        {/* <Button className={styles.button}>See Product</Button> */}
+        <Button>See Product</Button>
       </div>
       <div className={styles.item}>
         <Image src={heroImg} className={styles.img} />
