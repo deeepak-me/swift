ed8e0d435abf06bc31c8fbd740cd281a1ab28395
deeepak-me <deepak.krishnan.e@gmail.com>
2023-06-29 20:32:03 +0530

    feat: Reusable button Component completed

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,9 +1,61 @@
+"use client";
+
 import React from "react";
 
-import styles from "./button.module.css";
+import { styled, css } from "styled-components";
 
-const Button = () => {
-  return <button className={styles.button}>See Product</button>;
+const StyledButton = styled.button`
+  font-weight: 700;
+  font-size: 13px;
+  line-height: 18px;
+  letter-spacing: 1px;
+  background-color: #d87d4a;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  padding: 15px 30px;
+  width: 160px;
+  text-transform: uppercase;
+
+  &:hover {
+    opacity: 0.9;
+    background: #fbaf85;
+    /* color: red; */
+  }
+
+  ${(props) =>
+    props.primary &&
+    css`
+      background: #000;
+      color: #fff;
+
+      &:hover {
+        opacity: 0.9;
+        background: #fff;
+        color: #000;
+      }
+    `}
+
+  ${(props) =>
+    props.secondary &&
+    css`
+      display: flex;
+      justify-content: center;
+      border: none;
+      background: none;
+      text-align: center;
+      color: #000;
+      opacity: 0.5;
+
+      &:hover {
+        background: none;
+        color: #D87D4A; 
+      }
+    `}
+`;
+
+const Button = ({ children, ...rest }) => {
+  return <StyledButton {...rest}>{children}</StyledButton>;
 };
 
 export default Button;
