cd5c232287242ae8e0e9ef074ff3db699d919a5d
deeepak-me <deepak.krishnan.e@gmail.com>
2023-07-04 16:43:07 +0530

    feat: Simple card created successfully

---
+++ src/components/simplecard/Simplecard.jsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React from "react";
+import Button from "../button/Button";
+import { styled } from "styled-components";
+
+const Card = styled.div`
+  width: 540px;
+  height: 320px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  background: #f1f1f1;
+  display: flex;
+  align-items: center;
+`;
+
+const Info = styled.div`
+    margin-left: 95px;
+`;
+
+const Title = styled.h4`
+  color: #000;
+  font-size: 28px;
+  font-family: Manrope;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  margin-bottom: 32px;
+`;
+
+const Simplecard = () => {
+  return (
+    <Card>
+      <Info>
+        <Title>YX1 EARPHONES</Title>
+        <Button bordered>See Product</Button>
+      </Info>
+    </Card>
+  );
+};
+
+export default Simplecard;
