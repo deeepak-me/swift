681ace65aae020749a2ee1efbc97f5da1c8c0aa8
deeepak-me <deepak.krishnan.e@gmail.com>
2023-07-12 01:37:45 +0530

    feat: components wrapped with style sheet manager

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,37 @@
+"use client";
+
 import BrandCard from "@/components/brandcarrd/BrandCard";
 import { Hero } from "@/components/hero/Hero";
+import ImageCard from "@/components/imagecard/Imagecard";
 import LargeProductCard from "@/components/largeproductcard/Largeproductcard";
 import LongProductCard from "@/components/longproductcard/Longproductcard";
 import ProductCard from "@/components/productcard/Productcard";
 import SimpleCard from "@/components/simplecard/Simplecard";
+import { StyleSheetManager, styled } from "styled-components";
+import isPropValid from "@emotion/is-prop-valid";
 
+// const StyledHero = styled.div`
+//   background-color: #141414;
+// `;
+const StyledHero = styled(SimpleCard)`
+  background-color: #141414;
+  margin-left: 165px;
+`
 export default function Home() {
   return (
-    <div>
-      <Hero />
-      <ProductCard />
-      <LargeProductCard />
-      <LongProductCard/>
-      <SimpleCard />
-      {/* <BrandCard /> */}
-    </div>
+    <StyleSheetManager shouldForwardProp={isPropValid}>
+      <div>
+        {/* <StyledHero /> */}
+          <Hero />
+        {/* </StyledHero> */}
+        <ProductCard />
+        <LargeProductCard />
+        <LongProductCard />
+        <ImageCard />
+        <StyledHero />
+        {/* <SimpleCard /> */}
+        <BrandCard />
+      </div>
+    </StyleSheetManager>
   );
 }
