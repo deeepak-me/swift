dd67ba35a8f6f4913b9e752754f030faca2d1593
deeepak-me <deepak.krishnan.e@gmail.com>
2023-07-15 23:53:15 +0530

    feat : adding styled components

--- src/components/largeproductcard/Largeproductcard.jsx
+++ src/components/largeproductcard/Largeproductcard.jsx
@@ -4,24 +4,54 @@ import Image from "next/image";
 import styles from "./largeproductcard.module.css";
 import speaker from "public/speaker.png";
 import Button from "../button/Button";
+import { styled } from "styled-components";
+
+const Container = styled.div`
+  /* padding-left: 165px;
+  padding-right: 165px; */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 48px;
+`;
 
 const LargeProductCard = () => {
   return (
-    <div className={styles.info}>
-      <div className={styles.cardDesc}>
-        <div className={styles.item}>
-          <Image src={speaker} className={styles.img} />
-        </div>
-        <div className={styles.cardInfo}>
-          <h1 className={styles.title}>ZX9 <br /> SPEAKER</h1>
-          <p className={styles.para}>
-            Upgrade to premium speakers that are <br /> phenomenally built to deliver
-            truly remarkable <br /> sound.
-          </p>
-          <Button color="primary">See Product</Button>
+    <Container>
+      <div className={styles.info}>
+        <div className={styles.cardDesc}>
+          <div className={styles.item}>
+            <Image src={speaker} className={styles.img} />
+          </div>
+          <div className={styles.cardInfo}>
+            <h1 className={styles.title}>
+              ZX9 <br /> SPEAKER
+            </h1>
+            <p className={styles.para}>
+              Upgrade to premium speakers that are <br /> phenomenally built to
+              deliver truly remarkable <br /> sound.
+            </p>
+            <Button color="primary">See Product</Button>
+          </div>
         </div>
       </div>
-    </div>
+    </Container>
+
+    // <div className={styles.info}>
+    //   <div className={styles.cardDesc}>
+    //     <div className={styles.item}>
+    //       <Image src={speaker} className={styles.img} />
+    //     </div>
+    //     <div className={styles.cardInfo}>
+    //       <h1 className={styles.title}>ZX9 <br /> SPEAKER</h1>
+    //       <p className={styles.para}>
+    //         Upgrade to premium speakers that are <br /> phenomenally built to deliver
+    //         truly remarkable <br /> sound.
+    //       </p>
+    //       <Button color="primary">See Product</Button>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
