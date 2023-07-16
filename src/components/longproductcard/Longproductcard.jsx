faa5ffe42a8a69a0219382c7b1a41a61d079bde3
deeepak-me <deepak.krishnan.e@gmail.com>
2023-07-16 16:27:55 +0530

    feat: Implemented styled components

--- src/components/longproductcard/Longproductcard.jsx
+++ src/components/longproductcard/Longproductcard.jsx
@@ -6,16 +6,29 @@ import Image from "next/image";
 import productImg from "public/speaker2.png";
 import Button from "../button/Button";
 
-
 import { styled } from "styled-components";
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 48px;
+`;
+
 const Bg = styled.div`
-  background-image: url(${productImg});
+  /* background-image: url(${productImg}); */
+  width: 1110px;
+  height: 320px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  background: #f1f1f1;
+  display: flex;
+  align-items: center;
 `;
 const Title = styled.h4`
   color: #000;
   font-size: 28px;
-  font-family: Manrope;
+  /* font-family: Manrope; */
   font-weight: 700;
   letter-spacing: 2px;
   text-transform: uppercase;
@@ -27,17 +40,20 @@ const Info = styled.div`
   justify-content: center;
   align-items: flex-start;
   gap: 32px;
+  margin-left:95px;
 `;
 
 const LongProductCard = () => {
   return (
-    <Bg>
-      {/* <Image src={productImg} /> */}
-      <Info>
-        <Title>ZX7 SPEAKER</Title>
-        <Button variant="bordered">See Product</Button>
-      </Info>
-    </Bg>
+    <Container>
+      <Bg>
+        {/* <Image src={productImg} /> */}
+        <Info>
+          <Title>ZX7 SPEAKER</Title>
+          <Button variant="bordered">See Product</Button>
+        </Info>
+      </Bg>
+    </Container>
   );
 };
 
