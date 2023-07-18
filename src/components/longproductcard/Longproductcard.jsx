de0b70c555b6c0079dd6d949c228854cf19033c9
deeepak-me <deepak.krishnan.e@gmail.com>
2023-07-19 00:32:12 +0530

    feat: Implemented long product card .

--- src/components/longproductcard/Longproductcard.jsx
+++ src/components/longproductcard/Longproductcard.jsx
@@ -13,18 +13,27 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   margin-bottom: 48px;
+  position: relative;
+  /* position:absolute; */
+  /* padding-right: 165px;
+  padding-left: 165px; */
 `;
 
 const Bg = styled.div`
   /* background-image: url(${productImg}); */
-  width: 1110px;
-  height: 320px;
+  /* width: 1110px;
+  height: 320px; */
   flex-shrink: 0;
   border-radius: 8px;
-  background: #f1f1f1;
+  /* background: #f1f1f1; */
   display: flex;
   align-items: center;
 `;
+
+const Img = styled(Image)`
+  border-radius: 8px;
+  object-fit:cover;
+`
 const Title = styled.h4`
   color: #000;
   font-size: 28px;
@@ -40,14 +49,28 @@ const Info = styled.div`
   justify-content: center;
   align-items: flex-start;
   gap: 32px;
-  margin-left:95px;
+  margin-left: 95px;
+  /* position: relative; */
+  position: absolute;
+  /* left:-970px; */
 `;
 
 const LongProductCard = () => {
   return (
     <Container>
       <Bg>
-        {/* <Image src={productImg} /> */}
+        <Img src={productImg} />
+        {/* <Image
+          alt="ProductImg"
+          src={productImg}
+          placeholder="blur"
+          quality={100}
+          fill
+          // sizes="100vw"
+          style={{
+            objectFit: "cover",
+          }}
+        /> */}
         <Info>
           <Title>ZX7 SPEAKER</Title>
           <Button variant="bordered">See Product</Button>
