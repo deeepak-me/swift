fc66ab96afde4334d5e8308654f886cfd9e7de6f
deeepak-me <deepak.krishnan.e@gmail.com>
2023-09-08 16:08:57 +0530

    feat: Building Headphone Page

--- src/pages/Headphones.jsx
+++ src/pages/Headphones.jsx
@@ -11,6 +11,8 @@ import FeatureCard from "../components/featurecard/FeatureCard";
 import Cart from "../components/cart/Cart";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
+import ProductDetailsCard from "../components/productdetailscard/ProductDetailsCard";
+import ProductList from "../list/productlist/ProductList";
 
 const Headphones = () => {
   const [blogs, setBlogs] = useState([
@@ -21,27 +23,29 @@ const Headphones = () => {
         "The new XX99 Mark II headphones is the pinnacle of pristine audio.It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.",
       price: "$ 2,999",
     },
-    // {
-    //   image: headphoneImg,
-    //   title: "XX59 Headphones",
-    //   about:
-    //     "Enjoy your audio almost anywhere and customize it to your specific tastes with the XX59 headphones. The stylish yet durable versatile wireless headset is a brilliant companion at home or on the move.",
-    //   price: "$ 2,999",
-    // },
+    {
+      image: headphoneImg,
+      title: "XX59 Headphones",
+      about:
+        "Enjoy your audio almost anywhere and customize it to your specific tastes with the XX59 headphones. The stylish yet durable versatile wireless headset is a brilliant companion at home or on the move.",
+      price: "$ 2,999",
+    },
   ]);
 
   return (
     <div>
       <Navbar />
-      <TitleCard />
-      <Cart />
-      <PriceCard blogs={blogs} />
+      <TitleCard title="HEADPHONES"/>
+      <ProductList blogs={blogs} />
+      {/* <ProductDetailsCard blogs={blogs}  /> */}
+      {/* <Cart />
+      <PriceCard blogs={blogs} /> */}
       {/* <PriceCard blogs={blogs}/> */}
-      <FeatureCard />
+      {/* <FeatureCard />
       <SuggestionCard />
       <ProductCard />
       <BrandCard />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 };
