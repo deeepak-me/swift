d917231870834fcdaaa1eedfe52254ff422df683
deeepak-me <deepak.krishnan.e@gmail.com>
2023-09-08 16:15:31 +0530

    feat: Added two new Route path

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import Home from "./pages/Home/Home";
 import { StyleSheetManager } from "styled-components";
 import isPropValid from "@emotion/is-prop-valid";
-import Headphones from "./pages/Headphones";
 
 import { Route,Routes } from "react-router-dom";
+
+import Headphones from "./pages/Headphones";
+import Earphone from "./pages/Earphone";
+import Speakers from "./pages/Speakers";
 
 const App = () => {
   return (
@@ -15,7 +18,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home  />} />
           <Route path="/Headphones" element={<Headphones  />} />
-
+          <Route path="/Speakers" element={<Speakers  />} />
+          <Route path="/Earphones" element={<Earphone  />} />
         </Routes>
       </div>
     </StyleSheetManager>
