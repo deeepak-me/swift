dc70d5962e91794bdb28f654554d7c27015abcd1
deeepak-me <deepak.krishnan.e@gmail.com>
2023-10-03 19:17:49 +0530

    feat: cart card file created

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Top = styled.div`
+  display
+`;
 
 const Cart = () => {
   return (
-    <div>Cart</div>
-  )
-}
+    <div>
+      <div>
+        <h6>
+          CART (<span>3</span>)
+        </h6>
+        <Link to="!#">Remove All</Link>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
