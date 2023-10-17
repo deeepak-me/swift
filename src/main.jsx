6a2e7b11eb01cd26c3ade6dbde9473f0d079697a
deeepak-me <deepak.krishnan.e@gmail.com>
2023-10-18 01:57:35 +0530

    feat: wrapped with context provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";
+import CartContextProvider from "./context/cartContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CartContextProvider>
+        <App />
+      </CartContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
