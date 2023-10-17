784a34e1b596632e7de00471e1e1229bd986ef9d
deeepak-me <deepak.krishnan.e@gmail.com>
2023-10-18 01:54:45 +0530

    feat: card Reducer file created

---
+++ src/context/cartReducer.js
@@ -0,0 +1,21 @@
+const CartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_CART": {
+      console.log("Adding to cart:", action.payload);
+      return {
+        ...state,
+        cart: [...state.cart, action.payload],
+      };
+    }
+    case "REMOVE_FROM_CART": {
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.id !== action.payload.id),
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export default CartReducer;
